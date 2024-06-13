330162d3a4f27808f5dd85df596fba9b123a9521
Dzmitry Bitsiutski <dzmitry.bitsiutski@paysera.net>
2024-06-13 17:00:59 +0200

    Job worker

--- src/Worker/Job/JobWorker.php
+++ src/Worker/Job/JobWorker.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Paysera\RoadRunnerBundle\Worker\Job;
 
 use Emi\Common\Worker\Job\Event\Handler\EventHandlerInterface;
+use Paysera\RoadRunnerBundle\Worker\WorkerInterface;
 use Psr\Log\LoggerInterface;
 use Spiral\RoadRunner\Jobs\ConsumerInterface;
 use Spiral\RoadRunner\Jobs\Exception\JobsException;
